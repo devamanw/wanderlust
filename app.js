6ef48ef7d57f7d500290eb9f5c03411ffce00bc3
Aman upadhyay <devamanw@gmail.com>
2025-10-01 22:49:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -81,9 +81,6 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.get("/", (req, res) => {
- res.render("listings/index.ejs");
-});
 
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
