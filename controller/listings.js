8c681dbca6c3261a5fa0054b5ee5cce1a2802e1b
Aman upadhyay <devamanw@gmail.com>
2025-10-02 01:34:37 +0530

    Update listings.js

--- controller/listings.js
+++ controller/listings.js
@@ -87,22 +87,3 @@ module.exports.destroyListing = async (req, res) => {
   console.log(deletedListing);
   res.redirect("/listings");
 }
-
-// Search listings controller
-module.exports.searchListings = async (req, res) => {
-  const query = req.query.q;
-  if (!query) {
-    return res.redirect("/listings");
-  }
-
-  const results = await Listing.find({
-    $or: [
-      { title: { $regex: query, $options: "i" } },
-      { description: { $regex: query, $options: "i" } },
-      { location: { $regex: query, $options: "i" } }
-    ]
-  });
-
-  res.render("listings/search", { listings: results, searchQuery: query });
-};
-
